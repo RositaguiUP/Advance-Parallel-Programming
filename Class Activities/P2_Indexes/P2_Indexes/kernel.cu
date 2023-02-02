b73cd3fda8767ccb2d7dfa6c924091028ae4a884
Rosita Aguirre Plascencia <0225352@up.edu.mx>
2023-02-02 09:02:14 -0600

    Updating P2 add P3

--- Class Activities/P2_Indexes/P2_Indexes/kernel.cu
+++ Class Activities/P2_Indexes/P2_Indexes/kernel.cu
@@ -29,7 +29,7 @@ __global__ void idx_calc_2d(int* input)
     int threads_per_row = gridDim.x * blockDim.x;
     int row_offset = blockIdx.y * threads_per_row;
     int gid = tid + row_offset + block_offset;
-    printf("[DEVICE] gridDim.x: %d, blockIdx.x: %d, blockIdy.x: %d, threadIdx.x: %d, gId: %d, data: %d\n\r",
+    printf("[DEVICE] gridDim.x: %d, blockIdx.x: %d, blockIdx.y: %d, threadIdx.x: %d, gId: %d, data: %d\n\r",
         gridDim.x, blockIdx.x, blockIdx.y, tid, gid, input[gid]);
 }
 
@@ -43,13 +43,34 @@ __global__ void idx_calc_2d_2d(int* input)
     int row_offset = blockIdx.y * threads_per_row;
 
     int gid = tid + row_offset + block_offset;
-    printf("[DEVICE] gridDim.x: %d, blockIdx.x: %d, blockIdy.x: %d, threadidx.x: %d, gId: %d, data: %d\n\r",
+    printf("[DEVICE] gridDim.x: %d, blockIdx.x: %d, blockIdx.y: %d, threadidx.x: %d, gId: %d, data: %d\n\r",
         gridDim.x, blockIdx.x, blockIdx.y, tid, gid, input[gid]);
 }
 
+
+__global__ void idx_calc_3d(int* input)
+{
+    int tid = blockDim.x * (threadIdx.y + blockDim.y * threadIdx.z) + threadIdx.x;
+    int threads_per_block = blockDim.x * blockDim.y * blockDim.z;
+    int block_offset = blockIdx.x * threads_per_block;
+
+    int threads_per_row = gridDim.x * threads_per_block;
+    int threads_per_face = gridDim.y * threads_per_row;
+    int row_offset = blockIdx.y * threads_per_row;
+    int face_offset = blockIdx.z * threads_per_face;
+    
+    int gid = tid + row_offset + block_offset + face_offset;
+
+    // int bid = gridDim.x * (blockIdx.y + gridDim.y * blockIdx.z) + blockIdx.x;
+    // int gid = tid + bid * threads_per_block;
+    printf("[DEVICE] gridDim.x: %d, blockIdx.x: %d, blockIdx.y: %d, blockIdx.z: %d, threadidx.x: %d, gId: %d, data: %d\n\r",
+        gridDim.x, blockIdx.x, blockIdx.y, blockIdx.z, tid, gid, input[gid]);
+}
+
+
 int main()
 {
-    const int vectorSize = 32; // 16 16 16 32
+    const int vectorSize = 64; // 16 16 16 32
     int* dev_a;
 
     cudaMalloc((void**)&dev_a, vectorSize * sizeof(int));
@@ -65,13 +86,14 @@ int main()
 
     cudaMemcpy(dev_a, phost_a, vectorSize * sizeof(int), cudaMemcpyHostToDevice);
 
-    dim3 blockDim(4, 2); //  (16) (8) (4) (4,2);
-    dim3 gridDim(2, 2);  //  (1) (2) (2,2) (2,2);
+    dim3 blockDim(2, 2, 2); //  (16) (8) (4) (4,2);
+    dim3 gridDim(2, 2, 2);  //  (1) (2) (2,2) (2,2);
 
     // idx_calc_tid << < gridDim, blockDim >> > (dev_a);
     // idx_calc_gid << < gridDim, blockDim >> > (dev_a);
     // idx_calc_2d << < gridDim, blockDim >> > (dev_a);
-    idx_calc_2d_2d << < gridDim, blockDim >> > (dev_a);
+    // idx_calc_2d_2d << < gridDim, blockDim >> > (dev_a);
+    idx_calc_3d << < gridDim, blockDim >> > (dev_a);
 
     cudaDeviceSynchronize();
 
