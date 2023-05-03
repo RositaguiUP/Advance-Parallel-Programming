aa19a6d3bb19e517f10516f6fe7c14a54b810dd4
Rosita Aguirre Plascencia <0225352@up.edu.mx>
2023-05-02 20:04:05 -0600

    Updating P1

---
+++ Class Activities/1 Parcial/P6_Convolution/P6_Convolution/kernel.cu
@@ -0,0 +1,132 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+
+using namespace std;
+
+__global__ void convolution(int* a, int* k, int* newA, int xSize, int totSize)
+{
+    int tid = blockDim.x * (threadIdx.y + blockDim.y * threadIdx.z) + threadIdx.x;
+    int threads_per_block = blockDim.x * blockDim.y * blockDim.z;
+    int bid = gridDim.x * (blockIdx.y + gridDim.y * blockIdx.z) + blockIdx.x;
+    int gid = tid + bid * threads_per_block;
+
+    newA[gid] = 0;
+
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            int aPos = gid + i + j * xSize;
+            int side = gid % xSize == 0 ? -1 : (gid + 1) % xSize == 0 ? 1 : 0;
+
+            if (aPos >= 0 && aPos < totSize && ((side == -1 && (gid + 1 + i) % xSize != 0) || (side == 1 && (gid + i) % xSize != 0) || side == 0)) {
+                newA[gid] += a[aPos] * k[i + 1 + (j + 1) * 3];
+                /*
+                if (gid == 4) {
+                    printf("\naPos %d\ta[aPos] %d\tk[x-i, j-i] %d\ti %d\tj %d", aPos, a[aPos], k[i + 1 + (j + 1) * 3], i, j);
+                }*/
+            }
+        }
+    }
+}
+
+__global__ void convolution3d(int* a, int* k, int* newA, int xSize, int zSize, int totSize)
+{
+    int tid = blockDim.x * (threadIdx.y + blockDim.y * threadIdx.z) + threadIdx.x;
+    int threads_per_block = blockDim.x * blockDim.y * blockDim.z;
+    int bid = gridDim.x * (blockIdx.y + gridDim.y * blockIdx.z) + blockIdx.x;
+    int gid = tid + bid * threads_per_block;
+
+    newA[gid] = 0;
+
+    for (int i = -1; i <= 1; i++) {
+        for (int j = -1; j <= 1; j++) {
+            for (int k = -1; k <= 1; k++) {
+                int aPos = gid + i + j * xSize + k * zSize;
+                int side = gid % xSize == 0 ? -1 : (gid + 1) % xSize == 0 ? 1 : 0;
+                int sidez = gid % zSize == 0 ? -1 : (gid + 1) % zSize == 0 ? 1 : 0;
+
+                if (aPos >= 0 && aPos < totSize && ((side == -1 && (gid + 1 + i) % xSize != 0) || (side == 1 && (gid + i) % xSize != 0) || side == 0)
+                    && ((sidez == -1 && (gid + 1 + k) % zSize != 0) || (sidez == 1 && (gid + k) % zSize != 0) || sidez == 0)) {
+                    // newA[gid] += a[aPos] * k[i + 1 + (j + 1) * 3];
+                }
+            }
+        }
+    }
+}
+
+void printMatrix(int* a, int xSize, int totSize) {
+    for (int i = 0; i < totSize; i++) {
+        if (i % xSize == 0) {
+            printf("\n");
+        }
+        printf("  %d", a[i]);
+    }
+}
+
+int main()
+{
+    const int xSize = 8;
+    const int ySize = 8;
+    const int totSize = xSize* ySize;
+    const int size = totSize * sizeof(int);
+    int* dev_a, * dev_k, * dev_new;
+
+    cudaMalloc((void**)&dev_a, size);
+    cudaMalloc((void**)&dev_new, size);
+    cudaMalloc((void**)&dev_k, 9 * sizeof(int));
+
+    int* phost_a, * phost_k, * phost_new;
+
+    phost_a = (int*)malloc(size);
+    phost_new = (int*)malloc(size);
+    phost_k = (int*)malloc(9 * sizeof(int));
+
+    for (int i = 0; i < totSize; i++) {
+        phost_a[i] = i + 1; // rand() % 255;
+    }
+
+    // Filter matrix
+    for (int i = 0; i < 9; i++) {
+        phost_k[i] = 0;
+    }
+    phost_k[4] = 1;
+    
+    
+    cudaMemcpy(dev_a, phost_a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_k, phost_k, 9 * sizeof(int), cudaMemcpyHostToDevice);
+
+    printf("\n\n*****    MATRIX k    *****\n");
+    printMatrix(phost_k, 3, 9);
+
+    dim3 blockDim(xSize, ySize);
+    dim3 gridDim(1);
+
+    clock_t gpu_start, gpu_stop;
+
+    gpu_start = clock();
+    convolution << < gridDim, blockDim >> > (dev_a, dev_k, dev_new, xSize, totSize);
+    cudaDeviceSynchronize();
+
+    gpu_stop = clock();
+    double cps_gpu = (double)((double)(gpu_stop - gpu_start) / CLOCKS_PER_SEC);
+    printf("\n\nExecution Time [ET.GPU]: %4.6f\n\r", cps_gpu);
+
+    cudaMemcpy(phost_new, dev_new, size, cudaMemcpyDeviceToHost);
+
+    printf("\n\n*****    MATRIX A    *****\n");
+    printMatrix(phost_a, xSize, totSize);
+
+    printf("\n\n*****   MATRIX NEW   *****\n");
+    printMatrix(phost_new, xSize, totSize);
+
+    /*cudaDeviceReset();*/
+    cudaFree(dev_a);
+    cudaFree(dev_k);
+    cudaFree(dev_new);
+
+    return 0;
+}
---
+++ Class Activities/2 Parcial/Exam/Exam/kernel.cu
@@ -0,0 +1,222 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define N 1024
+
+struct patients {
+    int PatientID[N];
+    int age[N];
+    double glucosa[N];
+    double heart_rate[N];
+    double preassure_s[N];
+    double preassure_d[N];
+};
+
+void fillData(patients * data) {
+    for (int i = 0; i < N; i++) {
+        data->PatientID[i] = i;
+        data->age[i] = (rand() % (60 - 12 + 1)) + 12;
+        data->glucosa[i] = (rand() % (350 - 100 + 1)) + 100;
+        data->heart_rate[i] = (rand() % (170 - 90 + 1)) + 90;
+        data->preassure_s[i] = (rand() % (150 - 100 + 1)) + 100;
+        data->preassure_d[i] = (rand() % (90 - 70 + 1)) + 70;
+    }
+}
+
+__global__ void getMeanStdDev(patients* data, double* res_mean, double* res_std_dev) {
+    __shared__ patients s_data_m;
+    __shared__ patients s_data_sd;
+
+    int gid = (threadIdx.x + threadIdx.y * blockDim.x) + (blockIdx.x + blockIdx.y * gridDim.x) * (blockDim.x * blockDim.y);
+    int offset = N / 2;
+    
+
+    s_data_m = data[0];
+    s_data_sd = data[0];
+
+    __syncthreads();
+
+    // Mean
+    for (int i = offset; i > 0; i /= 2)
+    {
+        if (gid + i < N) {
+            s_data_m.age[gid] += s_data_m.age[gid + i];
+            s_data_m.glucosa[gid] += s_data_m.glucosa[gid + i];
+            s_data_m.heart_rate[gid]  += s_data_m.heart_rate[gid + i];
+            s_data_m.preassure_s[gid] += s_data_m.preassure_s[gid + i];
+            s_data_m.preassure_d[gid] += s_data_m.preassure_d[gid + i];
+        }
+        __syncthreads();
+    }
+
+    res_mean[0] = s_data_m.age[0] / N;
+    res_mean[1] = s_data_m.glucosa[0] / N;
+    res_mean[2] = s_data_m.heart_rate[0] / N;
+    res_mean[3] = s_data_m.preassure_s[0] / N;
+    res_mean[4] = s_data_m.preassure_d[0] / N;
+
+    // Std Dev
+
+    __syncthreads();
+
+    if (gid < N) {
+        s_data_sd.age[gid] = pow(s_data_sd.age[gid] - res_mean[0], 2);
+        s_data_sd.glucosa[gid] = pow(s_data_sd.glucosa[gid] - res_mean[1], 2);
+        s_data_sd.heart_rate[gid]  = pow(s_data_sd.heart_rate[gid] - res_mean[2], 2);
+        s_data_sd.preassure_s[gid] = pow(s_data_sd.preassure_s[gid] - res_mean[3], 2);
+        s_data_sd.preassure_d[gid] = pow(s_data_sd.preassure_d[gid] - res_mean[4], 2);
+    }
+    
+    __syncthreads();
+
+    for (int i = offset; i > 0; i /= 2)
+    {
+        if (gid + i < N) {
+            s_data_sd.age[gid] += s_data_sd.age[gid + i];
+            s_data_sd.glucosa[gid] += s_data_sd.glucosa[gid + i];
+            s_data_sd.heart_rate[gid]  += s_data_sd.heart_rate[gid + i];
+            s_data_sd.preassure_s[gid] += s_data_sd.preassure_s[gid + i];
+            s_data_sd.preassure_d[gid] += s_data_sd.preassure_d[gid + i];
+        }
+        __syncthreads();
+    }
+    
+    res_std_dev[0] = s_data_sd.age[0] / (N - 2);
+    res_std_dev[1] = s_data_sd.glucosa[0] / (N - 2);
+    res_std_dev[2] = s_data_sd.heart_rate[0] / (N - 2);
+    res_std_dev[3] = s_data_sd.preassure_s[0] / (N - 2);
+    res_std_dev[4] = s_data_sd.preassure_d[0] / (N - 2);
+
+}
+
+
+void getMeanStdDevCPU(patients data, double* res_mean, double* res_std_dev) {
+    // Mean
+    res_mean[0] = 0;
+    res_mean[1] = 0;
+    res_mean[2] = 0;
+    res_mean[3] = 0;
+    res_mean[4] = 0;
+
+    for (int i = 0; i < N; i++) {
+        res_mean[0] += data.age[i];
+        res_mean[1] += data.glucosa[i];
+        res_mean[2] += data.heart_rate[i];
+        res_mean[3] += data.preassure_s[i];
+        res_mean[4] += data.preassure_d[i];
+    }
+
+    res_mean[0] /= N;
+    res_mean[1] /= N;
+    res_mean[2] /= N;
+    res_mean[3] /= N;
+    res_mean[4] /= N;
+
+    // Std Dev
+    res_std_dev[0] = 0;
+    res_std_dev[1] = 0;
+    res_std_dev[2] = 0;
+    res_std_dev[3] = 0;
+    res_std_dev[4] = 0;
+
+    for (int i = 0; i < N; i++) {
+        res_std_dev[0] += pow(data.age[i] - res_mean[0], 2);
+        res_std_dev[1] += pow(data.glucosa[i] - res_mean[1], 2);
+        res_std_dev[2] += pow(data.heart_rate[i] - res_mean[2], 2);
+        res_std_dev[3] += pow(data.preassure_s[i] - res_mean[3], 2);
+        res_std_dev[4] += pow(data.preassure_d[i] - res_mean[4], 2);
+    }
+
+    res_std_dev[0] = sqrt(res_std_dev[0] / (N-2));
+    res_std_dev[1] = sqrt(res_std_dev[1] / (N-2));
+    res_std_dev[2] = sqrt(res_std_dev[2] / (N-2));
+    res_std_dev[3] = sqrt(res_std_dev[3] / (N-2));
+    res_std_dev[4] = sqrt(res_std_dev[4] / (N-2));
+}
+
+void printResults(patients *data, double *res_mean, double *res_std_dev) {
+    /*printf("\n\n ********** DATA **********\n");
+    for (int i = 0; i < N; i++) {
+        printf("\n\t %d = %d", i, data->age[i]);
+    }*/
+
+    printf("\n\n\t ********** MEAN RESULTS **********\n");
+    printf("\n\t\tAge           = %f", res_mean[0]);
+    printf("\n\t\tGlucosa       = %f", res_mean[1]);
+    printf("\n\t\tHeart Rate    = %f", res_mean[2]);
+    printf("\n\t\tPreassure S   = %f", res_mean[3]);
+    printf("\n\t\tPreassure D   = %f", res_mean[4]);
+                
+    printf("\n\n\t ********** STD DEV RESULTS **********\n");
+    printf("\n\t\tAge           = %f", res_std_dev[0]);
+    printf("\n\t\tGlucosa       = %f", res_std_dev[1]);
+    printf("\n\t\tHeart Rate    = %f", res_std_dev[2]);
+    printf("\n\t\tPreassure S   = %f", res_std_dev[3]);
+    printf("\n\t\tPreassure D   = %f\n\n", res_std_dev[4]);
+}
+
+int main()
+{
+    const int size_data = sizeof(patients);
+    const int size_res  = 5 * sizeof(double);
+    patients *dev_data;
+    double   *dev_res_mean;
+    double   *dev_res_std_dev;
+
+
+    cudaMalloc(&dev_data, size_data);
+    cudaMalloc(&dev_res_mean,  size_res);
+    cudaMalloc(&dev_res_std_dev,  size_res);
+
+
+    patients phost_data;
+    double   *phost_res_mean;
+    double   *phost_res_std_dev;
+    
+    
+    phost_res_mean = (double*)malloc(size_res);
+    phost_res_std_dev = (double*)malloc(size_res);
+
+    fillData(&phost_data);
+
+    cudaMemcpy(dev_data, &phost_data, size_data, cudaMemcpyHostToDevice);
+
+    dim3 blockDim(32,32);
+    dim3 gridDim(1);
+
+
+    // Implemention with parallel techniques
+    getMeanStdDev << < gridDim, blockDim >> >  (dev_data, dev_res_mean, dev_res_std_dev);
+    cudaDeviceSynchronize();
+
+    cudaMemcpy(phost_res_mean, dev_res_mean, size_res, cudaMemcpyDeviceToHost);
+    cudaMemcpy(phost_res_std_dev, dev_res_std_dev, size_res, cudaMemcpyDeviceToHost);
+
+    phost_res_std_dev[0] = sqrt(phost_res_std_dev[0]);
+    phost_res_std_dev[1] = sqrt(phost_res_std_dev[1]);
+    phost_res_std_dev[2] = sqrt(phost_res_std_dev[2]);
+    phost_res_std_dev[3] = sqrt(phost_res_std_dev[3]);
+    phost_res_std_dev[4] = sqrt(phost_res_std_dev[4]);
+    
+    printf("\n\n ******************** PARALLEL RESULTS ********************\n\n");
+    printResults(&phost_data, phost_res_mean, phost_res_std_dev);
+
+    // Secuencial calculus
+    getMeanStdDevCPU(phost_data, phost_res_mean, phost_res_std_dev);
+    
+    printf("\n\n ******************** NORMAL RESULTS ********************\n\n");
+    printResults(&phost_data, phost_res_mean, phost_res_std_dev);
+
+    // cudaFree(dev_data);
+    cudaFree(dev_res_mean);
+    cudaFree(dev_res_std_dev);
+
+    return 0;
+}
+
+
